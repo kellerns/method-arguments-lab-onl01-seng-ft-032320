fe7534f2fed0c7dbf412cc5eb4dd259ce0e44081
Nicholas Keller <nic.keller328@gmail.com>
2020-03-23 21:15:44 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -5,5 +5,5 @@ def introduction(name)
 end
 
 def introduction_with_language(name, lang)
-  puts "Hi, my name is #{name) and I am learning to program in #{lang}."
+  puts "Hi, my name is #{name)" + "and I am learning to program in #{lang}."
 end
